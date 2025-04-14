dd3019299ec4a326410c3725d620d3fe9ab6769d
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-04-14 14:20:15 +0300

    Added an alternative function to create a unique URL

--- helpers/createUniqueURL.js
+++ helpers/createUniqueURL.js
@@ -16,4 +16,23 @@ const createUniqueURL = async (Model, title) => {
     return newUrl;
 }
 
+// const createUniqueURL = async (Model, title, i = 0) => {
+//     const url = title
+//         .replace(/[^a-zA-Z\s]/g, '')
+//         .trim()
+//         .replace(/\s+/g, '-')
+//         .toLowerCase();
+
+//     const newUrl = i > 0 ? `${url}-${i}` : url;
+//     const isUrlExist = await Model.findOne({ url: newUrl });
+    
+//     console.log(i);
+
+//     if (isUrlExist) {
+//         return await createUniqueURL(Model, title, i + 1);
+//     }
+
+//     return newUrl;
+// }
+
 export default createUniqueURL;
