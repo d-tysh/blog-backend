49facd666cf28a847f3b38fd689191eec6a488ba
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-04-22 11:44:31 +0300

    Updated error handling in authenticate function

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -16,12 +16,12 @@ const authenticate = async (req, _, next) => {
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user || !user.token || user.token !== token) {
-            next(HttpError(401))
+            throw HttpError(401);
         }
         req.user = user;
         next();
     } catch (error) {
-        next(HttpError(401));
+        next(error);
     }
 }
 
